e599bd983e1db601922bd1664ba4e48e2a8d32e8
Radoslav Dorcik <dixie@dixie-laptop.(none)>
2009-01-06 22:15:18 +0100

    added sample configuration file

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Telnet host/port
+ATL_HOST="atlantis.talker.sk"
+ATL_PORT=23
+# JID = login into jabber / PWD = plaintext password
+JABBER_JID="meciar@example.com"
+JABBER_PWD="nbusr123"
+# This can be Set to None if JID contains all info
+JABBER_SERVER=("jabber.example.com", 5223)
+# JABBER_SERVER=None
